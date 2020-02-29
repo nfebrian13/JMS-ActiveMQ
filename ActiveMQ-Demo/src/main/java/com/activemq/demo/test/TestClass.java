1b39e9239f262cc5da6aa8c736d6842262ea3fd8
nfebrian13 <febrian.nana@gmail.com>
2020-02-29 17:36:49 +0700

    commit

---
+++ ActiveMQ-Demo/src/main/java/com/activemq/demo/test/TestClass.java
@@ -0,0 +1,31 @@
+package com.activemq.demo.test;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+import com.activemq.demo.conf.ConnectionConfig;
+import com.activemq.demo.conf.ConnectionFactories;
+import com.activemq.demo.conf.SessionConfig;
+import com.activemq.demo.sending.QueueSendingMessage;
+
+public class TestClass {
+
+	public void testSendingQueueMessage() throws JMSException {
+		ConnectionFactories connectionFactoryObj = new ConnectionFactories();
+		ConnectionConfig connectionConfObj = new ConnectionConfig();
+		SessionConfig sessionConfObj = new SessionConfig();
+		
+		QueueSendingMessage sendMessageObj = new QueueSendingMessage();
+		
+		/* sending message to Queue 1  */
+		ConnectionFactory cf = connectionFactoryObj.createConnectionFactory();
+		Connection conn = connectionConfObj.createConnection(cf);
+		Session session = sessionConfObj.createSession(conn);
+		sendMessageObj.sendTextMessageToQueue("Nana Test Message", session);
+		session.close();
+		conn.close();
+	}
+	
+}
